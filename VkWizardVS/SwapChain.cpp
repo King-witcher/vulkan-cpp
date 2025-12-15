a01b615db0736a56ca5f383a6a9e4c45ef27c0e5
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2025-12-14 22:22:24 -0300

    refactor swap chain

--- VkWizardVS/SwapChain.cpp
+++ VkWizardVS/SwapChain.cpp
@@ -31,71 +31,66 @@ vk::Extent2D chooseSwapExtent(vk::SurfaceCapabilitiesKHR capabilities, vk::Exten
 	return windowExtent;
 }
 
-vkwiz::SwapChain::SwapChain(Device& device, vk::raii::SurfaceKHR& surface, vk::Extent2D windowExtent) : device_(device)
+vkwiz::SwapChain::SwapChain(Device& device, vk::raii::SurfaceKHR& surface, vk::Extent2D windowExtent)
 {
+	auto& vkDevice = device.vkDevice();
 	auto swapChainSupport = device.querySwapchainSupportDetails(surface, windowExtent);
 	auto format = chooseSwapSurfaceFormat(swapChainSupport.formats);
 	auto presentMode = chooseSwapPresentMode(swapChainSupport.presentModes);
 	extent_ = chooseSwapExtent(swapChainSupport.capabilities, windowExtent);
+	vkImageFormat_ = format.format;
 	// TODO: considerar o surface capabilities
 	u32 minImageCount = swapChainSupport.capabilities.minImageCount + 1;
 
-	vk::SwapchainCreateInfoKHR swapChainCreateInfo{
-		.flags = vk::SwapchainCreateFlagsKHR(),
-		.surface = surface,
-		.minImageCount = minImageCount,
-		.imageFormat = format.format,
-		.imageColorSpace = format.colorSpace,
-		.imageExtent = extent_,
-		.imageArrayLayers = 1,
-		.imageUsage = vk::ImageUsageFlagBits::eColorAttachment,
-		.imageSharingMode = vk::SharingMode::eExclusive,
-		.preTransform = swapChainSupport.capabilities.currentTransform,
-		.compositeAlpha = vk::CompositeAlphaFlagBitsKHR::eOpaque,
-		.presentMode = presentMode,
-		.clipped = VK_TRUE,
-		.oldSwapchain = VK_NULL_HANDLE
-	};
+	vk::SwapchainCreateInfoKHR swapChainCreateInfo;
+	swapChainCreateInfo.setSurface(*surface);
+	swapChainCreateInfo.setMinImageCount(minImageCount);
+	swapChainCreateInfo.setImageFormat(vkImageFormat_);
+	swapChainCreateInfo.setImageColorSpace(format.colorSpace);
+	swapChainCreateInfo.setImageExtent(extent_);
+	swapChainCreateInfo.setImageArrayLayers(1);
+	swapChainCreateInfo.setImageUsage(vk::ImageUsageFlagBits::eColorAttachment);
+	swapChainCreateInfo.setImageSharingMode(vk::SharingMode::eExclusive);
+	swapChainCreateInfo.setPreTransform(swapChainSupport.capabilities.currentTransform);
+	swapChainCreateInfo.setCompositeAlpha(vk::CompositeAlphaFlagBitsKHR::eOpaque);
+	swapChainCreateInfo.setPresentMode(presentMode);
+	swapChainCreateInfo.setClipped(vk::True);
+	//swapChainCreateInfo.setOldSwapchain(VK_NULL_HANDLE);
+	vkSwapChain_ = vkDevice.createSwapchainKHR(swapChainCreateInfo);
 
-	swapChain = vk::raii::SwapchainKHR(device.vkDevice(), swapChainCreateInfo);
-	imageFormat_ = format.format;
-	images = swapChain.getImages();
-
-	createImageViews();
+	vkImages_ = vkSwapChain_.getImages();
+	vkImageViews_ = createImageViews(vkDevice);
 }
 
 std::tuple<vk::Image, vk::ImageView, u32> vkwiz::SwapChain::acquireImage(const vk::Semaphore semaphore, const vk::Fence fence)
 {
-	auto [result, index] = swapChain.acquireNextImage(UINT64_MAX, semaphore, fence);
+	auto [result, index] = vkSwapChain_.acquireNextImage(UINT64_MAX, semaphore, fence);
 	if (result != vk::Result::eSuccess && result != vk::Result::eSuboptimalKHR) {
 		throw runtime_error("Failed to acquire swap chain image!");
 	}
-	return { images[index], imageViews[index], index };
+	return { vkImages_[index], vkImageViews_[index], index };
 }
 
-void vkwiz::SwapChain::createImageViews()
+std::vector<vk::raii::ImageView> vkwiz::SwapChain::createImageViews(vk::raii::Device& vkDevice)
 {
-	auto device = &device_.vkDevice();
-	auto createInfo = vk::ImageViewCreateInfo{
-		.viewType = vk::ImageViewType::e2D,
-		.format = imageFormat_,
-		.components = {
-			.r = vk::ComponentSwizzle::eIdentity,
-			.g = vk::ComponentSwizzle::eIdentity,
-			.b = vk::ComponentSwizzle::eIdentity,
-			.a = vk::ComponentSwizzle::eIdentity,
-		},
-		.subresourceRange = {
-			.aspectMask = vk::ImageAspectFlagBits::eColor,
-			.baseMipLevel = 0,
-			.levelCount = 1,
-			.baseArrayLayer = 0,
-			.layerCount = 1,
-		},
-	};
-	imageViews.clear();
-	for (auto image : images) {
-		createInfo.image = image;
-		imageViews.emplace_back(*device, createInfo);
+	std::vector<vk::raii::ImageView> imageViews;
+	vk::ImageViewCreateInfo createInfo;
+	createInfo.setViewType(vk::ImageViewType::e2D);
+	createInfo.setFormat(vkImageFormat_);
+	createInfo.components.r = vk::ComponentSwizzle::eIdentity;
+	createInfo.components.g = vk::ComponentSwizzle::eIdentity;
+	createInfo.components.b = vk::ComponentSwizzle::eIdentity;
+	createInfo.components.a = vk::ComponentSwizzle::eIdentity;
+	createInfo.subresourceRange.setAspectMask(vk::ImageAspectFlagBits::eColor);
+	createInfo.subresourceRange.setBaseMipLevel(0);
+	createInfo.subresourceRange.setLevelCount(1);
+	createInfo.subresourceRange.setBaseArrayLayer(0);
+	createInfo.subresourceRange.setLayerCount(1);
+
+	for (auto image : vkImages_) {
+		createInfo.setImage(image);
+		imageViews.emplace_back(vkDevice, createInfo);
 	}
+
+	return imageViews;
 }
--- VkWizardVS/SwapChain.h
+++ VkWizardVS/SwapChain.h
@@ -10,22 +10,24 @@ namespace vkwiz {
 	{
 	public:
 		SwapChain(Device& device, vk::raii::SurfaceKHR& surface, vk::Extent2D windowExtent);
-		vk::Format imageFormat() const { return imageFormat_; }
-		vk::Image getImage(u32 index) const { return images[index]; }
-		vk::ImageView getImageView(u32 index) const { return imageViews[index]; }
-		vk::Extent2D extent() const { return extent_; }
+
 		std::tuple<vk::Image, vk::ImageView, u32> acquireImage(const vk::Semaphore semaphore, const vk::Fence fence);
 
-		vk::SwapchainKHR operator*() const { return *swapChain; }
+		vk::Image getImage(u32 index) const { return vkImages_[index]; }
+		vk::ImageView getImageView(u32 index) const { return vkImageViews_[index]; }
+
+		vk::Format imageFormat() const { return vkImageFormat_; }
+		vk::Extent2D extent() const { return extent_; }
+		vk::raii::SwapchainKHR& vkSwapChain() { return vkSwapChain_; }
+		vk::SwapchainKHR operator*() const { return *vkSwapChain_; }
 
 	private:
-		Device& device_;
-		vk::Format imageFormat_ = vk::Format::eUndefined;
+		vk::Format vkImageFormat_;
 		vk::Extent2D extent_;
-		vk::raii::SwapchainKHR swapChain = nullptr;
-		vector<vk::Image> images;
-		vector<vk::raii::ImageView> imageViews;
+		vk::raii::SwapchainKHR vkSwapChain_ = nullptr;
+		vector<vk::Image> vkImages_;
+		vector<vk::raii::ImageView> vkImageViews_;
 
-		void createImageViews();
+		std::vector<vk::raii::ImageView> createImageViews(vk::raii::Device& vkDevice);
 	};
 }
